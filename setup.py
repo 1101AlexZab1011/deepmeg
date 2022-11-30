dcbc8a23ae653a78999a5e0f8cb6a76c6d52cd0b
AlexZab <alexey.zabolotniy.main@yandex.ru>
2022-11-30 15:06:20 +0100

    readme (empty)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="alltools",
+    name="deepmeg",
     version="0.0.1",
     author="Alexey Zabolotniy",
     author_email="alexey.zabolotniy.main@yandex.ru",
     description="All the neccessary stuff",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/1101AlexZab1011/alltools",
+    url="https://github.com/1101AlexZab1011/deepmeg",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
