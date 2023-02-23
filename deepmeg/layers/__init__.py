973244c89e398b11b92ebb8aff12b56386f0ed0d
AlexZab <alexey.zabolotniy.main@yandex.ru>
2023-02-23 14:22:22 +0100

    canonical spirit

--- deepmeg/layers/__init__.py
+++ deepmeg/layers/__init__.py
@@ -219,3 +219,77 @@ class FourieCov1D(nn.Module):
                 out_tensor[:, n_window, n_neuron] = torch.squeeze(cov, -1) + self.bias[n_neuron] if self.bias is not None else torch.squeeze(cov, -1)
 
         return out_tensor
+
+
+class CanonicalCov1D(nn.Module):
+
+    def __init__(self, out_channels, window_size, latent_dim = None, overlap=0, stride=1, padding=0, bias=True):
+        super().__init__()
+        self.window_size = window_size
+        self.latent_dim = latent_dim if latent_dim is not None else window_size // 2
+        self.overlap = overlap
+        self.stride = stride
+        self.padding = padding
+        self.weight1 = nn.Parameter(torch.empty(self.window_size, self.latent_dim, out_channels))
+        self.weight2 = nn.Parameter(torch.empty(self.window_size, self.latent_dim, out_channels))
+
+        if bias:
+            self.bias = nn.Parameter(torch.empty(out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            if fan_in != 0:
+                bound = 1 / math.sqrt(fan_in)
+                init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, X):
+        """Forward pass of the model.
+
+        Args:
+            input_ (torch.Tensor): The input tensor.
+
+        Returns:
+            torch.Tensor: The output tensor.
+        """
+        # inputs shape (N_batch, width) ~ (N_batch, n_times)
+        X_part1 = X[:, :-(self.window_size - self.overlap)] # shape: (batches, times)
+        X_part2 = X[:, self.window_size - self.overlap:,]
+
+        assert X_part1.shape == X_part2.shape, f'Windowed sequence is corrupted: {X_part1.shape} vs {X_part2.shape}'
+
+        convargs = ((X_part1.shape[1], X_part1.shape[0]), self.window_size, self.padding, self.stride)
+        n_windows = compute_output_shape(*convargs)[0]
+        out_tensor = torch.zeros(X.shape[0], n_windows, self.weight.shape[-1]) # ~ batches x n_windows x out_channels
+
+        # iter over neurons
+        for n_neuron in range(self.weight.shape[-1]):
+            for p, q, k in conviter(*convargs):
+                # p shape: n_times, ...
+                # q shape: n_windows, 0
+                # k shape: window_size, ...
+                time_range = p[0]
+                window_range = k[0]
+                n_window = q[0]
+
+                window1 = X_part1[:, time_range]
+                window2 = X_part2[:, time_range]
+                weight1 = self.weight1[window_range, :, n_neuron]
+                weight2 = self.weight2[window_range, :, n_neuron]
+
+                proj1 = torch.matmul(window1, weight1) # (N batch x time_range) @ (window_range x latent_dim x n_neuron) = (N batch x latent_dim)
+                proj2 = torch.matmul(window2, weight2)
+                cov = torch.mean(
+                    (proj1 - torch.mean(proj1, 1, keepdim=True))*
+                    (proj2 - torch.mean(proj2, 1, keepdim=True)),
+                    1,
+                    keepdim=True
+                ) # ~ (N_batch, 1)
+                out_tensor[:, n_window, n_neuron] = torch.squeeze(cov, -1) + self.bias[n_neuron] if self.bias is not None else torch.squeeze(cov, -1)
+
+        return out_tensor
--- deepmeg/models/experimental.py
+++ deepmeg/models/experimental.py
@@ -1,7 +1,7 @@
 from . import BaseModel
 import torch
 import torch.nn as nn
-from ..layers import FourieCov1D
+from ..layers import FourieCov1D, AutoCov1D, CanonicalCov1D
 from ..utils.convtools import conviter
 
 class FourierSPIRIT(BaseModel):
@@ -50,6 +50,92 @@ class FourierSPIRIT(BaseModel):
 
 
     def forward(self, x: torch.Tensor):
+        x = self.unmixing_layer(x)
+        x = self.temp_conv(x)
+        x_est = torch.stack(
+            [
+                timesel(x[:, i, :]) for i, timesel in enumerate(self.timesel_list)
+            ],
+            1
+        )
+
+        x_est = torch.squeeze(x_est, -1)
+        x_est = self.expander(x_est)
+
+        x_out = list()
+        for i, window in enumerate(self.windows):
+            x_out.append(
+                x[:, :, window]
+                *torch.unsqueeze(x_est[:, :, i], -1)
+            )
+
+        x = torch.cat(x_out, -1)
+
+        x = torch.stack(
+            [
+                timesel(x[:, i, :]) for i, timesel in enumerate(self.timecomp_list)
+            ],
+            1
+        )
+        x = torch.flatten(x, 1)
+        return self.fc_layer(x)
+
+
+class CanonicalSPIRIT(BaseModel):
+    def __init__(
+        self,
+        n_channels: int,
+        n_latent: int,
+        n_times: int,
+        filter_size: int,
+        window_size: int,
+        latent_dim: int,
+        pool_factor: int,
+        n_outputs: int
+    ):
+        super().__init__()
+        self.unmixing_layer = nn.Conv1d(n_channels, n_latent, kernel_size=1, bias=True)
+        self.temp_conv = nn.Conv1d(n_latent, n_latent, kernel_size=filter_size, bias=True, groups=n_latent, padding='same')
+        self.timesel_list = nn.ModuleList([
+            nn.Sequential(
+                CanonicalCov1D(1, window_size, latent_dim, 0, window_size, bias=True),
+            )
+            for _ in range(n_latent)
+        ])
+        self.expander = nn.Conv1d(
+            in_channels=8,
+            out_channels=8,
+            kernel_size=(2,),
+            padding=1
+        )
+        self.windows = [win for win, _, _ in conviter((n_times-(window_size),), window_size, 0, window_size)]
+        self.windows.append(slice(self.windows[-1].start + window_size, None))
+        self.timecomp_list = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(
+                    n_times,
+                    (n_times//pool_factor),
+                    bias=False
+                ),
+                nn.Dropout(.25),
+                torch.nn.Sigmoid()
+            )
+            for i in range(n_latent)
+        ])
+        final_out_features = (n_times//pool_factor)*n_latent
+        self.fc_layer = nn.Linear(final_out_features, n_outputs)
+
+
+    def forward(self, x: torch.Tensor):
+        """
+        Forward pass of the model.
+
+        Args:
+            x (torch.Tensor): Input tensor with shape (batch_size, n_channels, n_times).
+
+        Returns:
+            torch.Tensor: Output tensor with shape (batch_size, n_outputs).
+        """
         x = self.unmixing_layer(x)
         x = self.temp_conv(x)
         x_est = torch.stack(
